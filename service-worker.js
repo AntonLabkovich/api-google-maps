0cee3e9d47abc2f238e6b4d953daf5cee276ce67
AntonLabkovich <sharcnatch@gmail.com>
2020-07-22 02:23:28 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/api-google-maps/precache-manifest.5bd075160d416f867bb5318c11011c83.js"
+  "/api-google-maps/precache-manifest.eb83e2674dd1b7c2b799030e557eb7e0.js"
 );
 
 self.addEventListener('message', (event) => {
