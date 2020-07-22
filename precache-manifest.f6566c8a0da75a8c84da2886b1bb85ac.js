5921e5c59f3b6c94f4ad34f3062e5cefc0b7b7ea
AntonLabkovich <sharcnatch@gmail.com>
2020-07-22 23:05:24 +0300

    Updates

--- precache-manifest.f6566c8a0da75a8c84da2886b1bb85ac.js
+++ precache-manifest.f6566c8a0da75a8c84da2886b1bb85ac.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b06afe3e48bc1ca007ce1b6bc28cd364",
+    "revision": "690da22c0fdadd5fce5d1cb0eafdabae",
     "url": "/api-google-maps/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/api-google-maps/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "877e257c2d1a1247d70f",
+    "revision": "708116fe4f06522f04b2",
     "url": "/api-google-maps/static/css/main.36dc3170.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/api-google-maps/static/js/2.e214c061.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "877e257c2d1a1247d70f",
-    "url": "/api-google-maps/static/js/main.f622f4a2.chunk.js"
+    "revision": "708116fe4f06522f04b2",
+    "url": "/api-google-maps/static/js/main.81775d71.chunk.js"
   },
   {
     "revision": "766bb40b896be32dee4a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/api-google-maps/precache-manifest.7f67c1e93cf0db31122a96a8f928f66b.js"
+  "/api-google-maps/precache-manifest.f6566c8a0da75a8c84da2886b1bb85ac.js"
 );
 
 self.addEventListener('message', (event) => {
